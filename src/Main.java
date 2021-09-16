d0aff7e815bf336d32a86292c74c8bd35ba2d8a0
Victor Milhomem <64241639+VictorMilhomem@users.noreply.github.com>
2021-09-16 16:47:48 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args)
     {
-        JFrame frame = new TextEditorGui("Editor de Texto");
+        JFrame frame = new TextEditorGui("Notepad");
         frame.setBounds(0, 0, 700, 700);
         frame.setVisible(true);
         frame.setResizable(false);
